3405dd642c272f00cc54815bd60cfe3157945fcf
oguchi nzekwe <n.zekwe@gmail.com>
2022-12-28 15:30:32 +0100

    complete exercises from variables and data types

---
+++ variables_and_datatypes/exercise.js
@@ -0,0 +1,31 @@
+/**
+ * I now have a coding exercise for you to complete.
+ * I want you to edit this code such that the value of result is the average of 
+ * the numerical values of a, b,c and d, rounded up to the nearest whole number. 
+ * Note the variables are currently strings, but I want the average of the numerical values.
+ * Do this without editing the values that a, b, c and d are assigned.
+ * 
+ * Next I want you to assign the value of a random die roll to randomDieRoll. 
+ * A die of course has six sides, so the result should be created by generating 
+ * a random number between 1 and 6, inclusive.
+ * 
+ * The solution is shown in the next video lecture.
+ * 
+ * var a = ‘10’;
+ * var b = ‘5’;
+ * var c = ‘20’;
+ * var d = ‘30’;
+ * var result;
+ * var randomDieRollOutcome;
+ */
+
+const a = '10';
+const b = '5';
+const c = '20';
+const d = '30';
+
+const result = Math.ceil((Number(a), Number(b), Number(c), Number(d) / 4));
+console.log(result) // 8
+
+let randomDieRollOutcome = (Math.floor(Math.random() * 6) + 1);
+console.log(randomDieRollOutcome)
