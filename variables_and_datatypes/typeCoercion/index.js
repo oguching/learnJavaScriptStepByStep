1aa14696a1d32489061975db92ad514fbeb534c4
oguchi nzekwe <n.zekwe@gmail.com>
2022-12-28 15:05:32 +0100

    Add type coercion section.

---
+++ variables_and_datatypes/typeCoercion/index.js
@@ -0,0 +1,14 @@
+/**
+ * Type Coercion
+ * string + number = string
+ * number + string = string
+ * 
+ * In text inputs, the value of the inputs are always strings
+ * If we want to enter numbers and use numbers we have to wrap
+ * the input values in Number() 
+ * For example if we entered 11 then we wrap the value in Number()
+ * like so: Number(11) in our code.
+ */
+
+console.log('hello' + 5)
+// output: 'hello5'
