91b621c82015867f74ffd6916c9e8d7d1a721d38
oguchi nzekwe <n.zekwe@gmail.com>
2022-12-29 11:18:52 +0100

    add index.js

---
+++ boolean_logic/index.js
@@ -0,0 +1,12 @@
+/**
+ * && and
+ * If both operands are truthy, the expression will evaluate to the second operand.
+ * For example console.log(true && 1) will evaluate to 1 since both 1 and true are
+ * truthy.
+ * If either of the operands are falsy the operation evaluates to the first falsy
+ * operand.
+ * || or
+ * If either of the operands are truthy, then evaluate to the first truthy value.
+ * So console.log( 1 || 0) evaluates to 1.
+ * If they are both falsy evaluate to the second operand.
+ */
